84645d9a5446edcc43acad5d10a30defb7f3396a
Vina Fajar Rizqi Edhipeny <vinafjr5@gmail.com>
2019-04-10 23:34:59 +0700

    Display answers for question

---
+++ resources/views/answers/_create.blade.php
@@ -0,0 +1,25 @@
+    <div class="row mt-4">
+    <div class="col-md-12">
+            <div class="card">
+                 <div class="card-body">
+                <div class="card-title">
+                   <h3>Your Answer</h3>
+                   </div>
+                   <hr>
+                  <form action="{{ route('questions.answers.store', $question->id) }}" method="post">
+                    @csrf
+                    <div class="form-group">
+                      <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" rows="7" name="body"></textarea>
+                     <div class="invalid-feedback">
+                      <strong>@if ($errors->has('body'))</strong>
+                     </div>
+                      @endif
+                    </div> <br>
+                    <div class="form-group">
+                     <button type="submit" class="btn btn-lg btn-outline-primary">Submit</button>
+                    </div>
+                  </form>
+                </div>
+            </div>
+        </div>
+    </div>
