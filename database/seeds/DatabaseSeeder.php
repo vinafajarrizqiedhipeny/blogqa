27b88251e36c12995ee9bd71091587e998f2ea6c
Vina Fajar Rizqi Edhipeny <vinafjr5@gmail.com>
2019-04-30 11:46:47 +0700

    Finish

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        $this->call([
-       		UsersQuestionsTableSeeder::class,
+       		UsersQuestionsAnswersTableSeeder::class,
        		FavoritesTableSeeder::class,
        		VotablesTableSeeder::class,
-       ])
+       ]);
     }
 }
